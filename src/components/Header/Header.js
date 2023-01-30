4e901b36a637ac13eb1c4cd8d9b48613f4386af7
FrancoTalles <francotallesextreme@gmail.com>
2023-01-30 01:55:53 -0400

    refactor/HeaderHome

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
 import { HeaderStyle, LogoDivStyle } from "./styled";
 import logo from "../../assets/logo_xablau.png";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 export default function Header(props) {
+  const { user } = useContext(UserContext);
+  
   return (
     <HeaderStyle>
-      <img src={logo} alt="Xablau Store" />
+      <Link to="/">
+        <img src={logo} alt="Xablau Store" />
+      </Link>
       <LogoDivStyle>
         <h1>{props.carrinho?.length}</h1>
         <Link to="/carrinho">
           <ion-icon name="cart"></ion-icon>
         </Link>
         <Link to="/signin">
-          <ion-icon name="log-out"></ion-icon>
+          {JSON.stringify(user) === "{}" ? (
+            <ion-icon name="person"></ion-icon>
+          ) : (
+            <ion-icon name="log-out"></ion-icon>
+          )}
         </Link>
       </LogoDivStyle>
     </HeaderStyle>
